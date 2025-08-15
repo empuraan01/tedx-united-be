8d2ab7eede97ff145b067ee2961b37fdaa25931f
Padmanabhan <padmanabhansridhar2005@gmail.com>
2025-08-15 16:49:02 +0530

    please work

--- config/googlesheets.js
+++ config/googlesheets.js
@@ -1,13 +1,25 @@
 const { google } = require('googleapis');
-const path = require('path');
 
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
-const KEY_FILE_PATH = path.join(__dirname, 'credentials.json');
 
 async function getGoogleSheetsClient() {
     try {
+
+        const credentials = {
+            type: process.env.GOOGLE_SERVICE_ACCOUNT_TYPE,
+            project_id: process.env.GOOGLE_PROJECT_ID,
+            private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+            private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+            client_email: process.env.GOOGLE_CLIENT_EMAIL,
+            client_id: process.env.GOOGLE_CLIENT_ID,
+            auth_uri: process.env.GOOGLE_AUTH_URI,
+            token_uri: process.env.GOOGLE_TOKEN_URI,
+            auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+            client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL
+        };
+
         const auth = new google.auth.GoogleAuth({
-            keyFile: KEY_FILE_PATH,
+            credentials: credentials,
             scopes: SCOPES,
         });
         const authClient = await auth.getClient();
